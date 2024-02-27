8470bf4f1f73a2a759f4fc2fd261bda1c8bae165
Onyx <shadowangel404@proton.me>
2024-02-27 14:31:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,34 @@
-def set_pin():
-    global t_, npin
-    serial.write_line("setting pin")
-    while input.button_is_pressed(Button.AB):
-        pass
-    t_ = input.running_time()
-    while True:
-        if input.running_time() - t_ > 3:
-            if Math.round(input.running_time() / 100) % 10 == 5:
-                led.set_brightness(255)
-                soroban.show_number(npin)
-            elif Math.round(input.running_time() / 100) % 10 == 0:
-                led.set_brightness(127)
-                soroban.show_number(npin)
-            if input.running_time() - t_ > 5:
-                break
-            if input.button_is_pressed(Button.A) or input.button_is_pressed(Button.B):
-                t_ = input.running_time()
-        else:
-            if input.button_is_pressed(Button.A):
-                serial.write_line("Button A pressed")
-                npin += 1
-                soroban.show_number(npin)
-                t_ = input.running_time()
-            if input.button_is_pressed(Button.B):
-                t_ = input.running_time()
-                serial.write_line("Button B pressed")
-                npin = 0
-                soroban.show_number(npin)
-mode = 0
-npin = 0
-t_ = 0
-serial.write_line("started...")
+# Initialisation des variables
+pin_mode = "read"  # Mode initial
+selected_pin = 0   # Pin initial sélectionné
+pin_value = 0      # Valeur initiale du pin
 
-def on_forever():
-    global mode
-    if input.logo_is_pressed():
-        mode = (mode + 1) % 1
-    if input.button_is_pressed(Button.AB):
-        set_pin()
-basic.forever(on_forever)
+# Boucle principale
+while True:
+    # Mode de lecture
+    if pin_mode == "read":
+        pin_value = eval('pins.digital_read_pin(Digital.P%s)'%selected_pin)
+        display.show(str(pin_value))
+        sleep(1000)
+    
+    # Mode de configuration
+    elif pin_mode == "set":
+        display.show(str(selected_pin))
+        if button_a.was_pressed():
+            selected_pin -= 1
+            if selected_pin < 0:
+                selected_pin = 1
+            display.show(str(selected_pin))
+            sleep(500)
+        elif button_b.was_pressed():
+            selected_pin += 1
+            if selected_pin > 1:
+                selected_pin = 0
+            display.show(str(selected_pin))
+            sleep(500)
+        elif button_a.is_pressed() and button_b.is_pressed():
+            pin_mode = "read"
+            sleep(500)
+    
+    # Délai pour éviter les rebonds des boutons
+    sleep(20)
